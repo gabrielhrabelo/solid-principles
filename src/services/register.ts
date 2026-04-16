646be25a6cfb8abd58bd7e518809774acbba9828
Gabriel Henrique <gabrielrabeloti@gmail.com>
2026-04-16 15:26:30 -0300

    separated user register service from the http layer

---
+++ src/services/register.ts
@@ -0,0 +1,31 @@
+import { hash } from "bcryptjs"
+import { prisma } from "@/lib/prisma"
+
+interface RegisterServiceRequest {
+  name: string,
+  email: string, 
+  password: string
+}
+
+export async function registerService({ name, email, password }: RegisterServiceRequest) {
+
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email, 
+    }
+  })
+
+  if (userWithSameEmail) {
+    throw new Error("email already exists")
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash
+    }
+  })
+}
