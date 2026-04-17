aefedf6a2737f7f71047a9d22e519058bd46ef93
Gabriel Henrique <gabrielrabeloti@gmail.com>
2026-04-16 21:54:55 -0300

    implemented global errors handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import z, { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({ message: 'Validation error', issues: z.treeifyError(error) })
+  }
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to an external tool like Datadog / NewRelic / Sentry
+  }
+  return reply.status(500).send({ message: "internal server error" })
+})
