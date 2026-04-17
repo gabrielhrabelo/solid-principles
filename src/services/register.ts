a04d6ed21560e1069cd2f7d75cc95c94655d9720
Gabriel Henrique <gabrielrabeloti@gmail.com>
2026-04-16 21:36:21 -0300

    error improved on creating an user

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,5 +1,6 @@
 import { hash } from "bcryptjs"
 import type { UsersRepository } from "@/repositories/prisma/users-repository"
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error"
 
 interface RegisterServiceRequest {
   name: string,
@@ -20,7 +21,7 @@ export class RegisterService {
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw new Error("email already exists")
+      throw new UserAlreadyExistsError()
     }
 
     await this.usersRepository.create({
