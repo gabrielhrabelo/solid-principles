a81bb7bb3a10b21379a10436b5b82f7899fa6135
Gabriel Henrique <gabrielrabeloti@gmail.com>
2026-04-27 01:22:41 -0300

    introduced tdd and mocking concepts

--- src/repositories/prisma/check-ins-repository.ts
+++ src/repositories/prisma/check-ins-repository.ts
@@ -3,4 +3,5 @@ import type { Checkin } from "../../../generated/prisma/client";
 
 export interface CheckInsRepository {
   create(data: Prisma.CheckinUncheckedCreateInput): Promise<Checkin>
+  findByUserIdOnDate(userId: string, date: Date): Promise<Checkin | null>
 }
