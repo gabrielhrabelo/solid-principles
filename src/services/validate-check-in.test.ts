beb2907c94b594182db069e051a7049a9f201d7c
gabrielhrabelo <gabrielrabeloti@gmail.com>
2026-04-30 16:34:38 -0300

    created checkin late validation for a business rule

--- src/services/validate-check-in.test.ts
+++ src/services/validate-check-in.test.ts
@@ -39,4 +39,23 @@ describe('Valida check-in service', () => {
       }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
   })
+
+  it('should not be able to valida check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2025, 0, 1, 13, 40))
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
 })
