420ec29f8b0b33ac57dea939b7e8a4479cc36e92
Gabriel Henrique Rabelo Silva <gabrielhrabelosl@gmail.com>
2026-05-05 16:16:02 -0300

    Implemented profile controller with jwt

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,14 @@
+import { makeGetUserProfileService } from '@/services/factories/make-get-user-profile'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify()
 
-  return reply.status(200).send({ message: 'nenhuma mensagem por enquanto' })
+  const getUserProfile = makeGetUserProfileService()
+
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.status(200).send({ user })
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,12 +1,13 @@
 import type { FastifyInstance } from 'fastify'
-import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
+import { register } from './controllers/register'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
   // Authenticated
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
