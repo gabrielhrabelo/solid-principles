0ccd0fac1385836341863faf55aa94624b7ef50f
Gabriel Henrique Rabelo Silva <gabrielhrabelosl@gmail.com>
2026-05-05 18:08:57 -0300

    added test environment on vitest and prisma

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,40 @@
+import { prisma } from '@/lib/prisma'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+import type { Environment } from 'vitest/environments'
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Provide a DATABASE_URL env variable')
+  }
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
+export default (<Environment>{
+  name: 'prisma',
+  transformMode: 'ssr',
+  async setup() {
+    const schema = randomUUID()
+    const databaseUrl = generateDatabaseUrl(schema)
+
+    console.log(databaseUrl)
+
+    execSync('pnpx prisma db push')
+
+    process.env.DATABASE_URL = databaseUrl
+    return {
+      async teardown() {
+        // erase database
+        await prisma.$executeRawUnsafe(
+          'DROP SCHEMA IF EXISTS "${schema}" CASCADE',
+        )
+        await prisma.$disconnect()
+      },
+    }
+  },
+})
---
+++ src/http/controllers/register.test.ts
@@ -0,0 +1,5 @@
+import { test, expect } from "vitest";
+
+test("testing", () => {
+  expect(1 + 1).toEqual(2);
+});
