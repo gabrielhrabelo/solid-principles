0b14c1d3dfa59b1dec065cd414dcb27ec98bfc2b
Gabriel Henrique Rabelo Silva <gabrielhrabelosl@gmail.com>
2026-05-05 18:23:58 -0300

    added register e2e test

--- src/http/controllers/register.test.ts
+++ src/http/controllers/register.test.ts
@@ -1,5 +1,22 @@
-import { test, expect } from "vitest";
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '@/app'
 
-test("testing", () => {
-  expect(1 + 1).toEqual(2);
-});
+describe('register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Gabrielito',
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    })
+    expect(response.statusCode).toBe(201)
+  })
+})
